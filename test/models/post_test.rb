8f5f6ce1fb95342c410f10228c40a95d8a9a8bf1
Mihir Kumar Thakur <mihirthakur.cse@gmail.com>
2021-11-28 11:07:46 +0530

    Adds more test on post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid post' do
+    post = Post.new(title: 'Test title', body: 'body', user_id: User.first.id)
+    assert post.valid?
+  end
+
+
+  test 'invalid without title' do
+    post = Post.new(body: 'body', user_id: User.first.id)
+    refute post.valid?, 'post is invalid without title'
+  end
 end
